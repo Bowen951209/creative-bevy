8cdc38a509ae42c3500a88c733fb69bdf5c8d403
bowen951209 <bowen951209@gmail.com>
2025-08-04 12:51:00 +0800

    Black background & exit on esc pressed.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new()
+        .insert_resource(ClearColor(Color::BLACK))
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, hello)
+        .add_systems(Update, exit_on_esc)
+        .run();
+}
+
+fn hello() {
+    println!("Hello, Bevy!");
+}
+
+fn exit_on_esc(keyboard_input: Res<ButtonInput<KeyCode>>, mut exit: EventWriter<AppExit>) {
+    if keyboard_input.just_pressed(KeyCode::Escape) {
+        exit.write(AppExit::Success);
+    }
 }
